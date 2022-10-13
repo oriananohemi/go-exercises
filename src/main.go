e3827da75092ef0a429484d885dde0c307ce9f07
Oriana Reina <oriana.nohemi22@gmail.com>
2022-10-12 21:49:49 -0500

    declaration

--- src/main.go
+++ src/main.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración
+	const pi float64 = 3.1416
+	const pi2 = 3.1416
+	base := 5 // declara y asigna la variable
+	var altura int = 14
+	var rango int
+
+	// Las variables tienen un valor por defecto, no se asigna null
+	var a int     // 0
+	var b float64 // 0
+	var c string  // " "
+	var d bool    // false
+
+	fmt.Println(a, b, c, d)
 
 	fmt.Println("Hola mundo")
 
