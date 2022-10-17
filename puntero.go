8b346cce60ba131c205a3dca4888e414da9df6ab
Oriana Reina <oriana.nohemi22@gmail.com>
2022-10-17 13:05:03 -0500

    update reference

--- puntero.go
+++ puntero.go
@@ -2,6 +2,12 @@ package puntero
 
 import "fmt"
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
 func puntero() {
 	a := 50
 	// el & es para acceder al lugar en memoria
@@ -13,4 +19,21 @@ func puntero() {
 
 	// Si modificamos una variable con *, todas las variables que esten aputando a este lugar cambiaran
 	*b = 100
+}
+
+// Actualiza la ram
+func (myPC *pc) duplicateRAM() {
+	myPC.ram = myPC.ram * 2
+}
+
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "Pong")
+}
+
+func main() {
+	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+
+	myPC.ping()
+
+	myPC.duplicateRAM()
 }
